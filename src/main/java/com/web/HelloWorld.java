ca2742ebc24cde5521472edde84c077df17fd6eb
flyboss <915741005@qq.com>
2018-04-28 22:15:10 +0800

    add HelloWorld in web

---
+++ src/main/java/com/web/HelloWorld.java
@@ -0,0 +1,28 @@
+package com.web;
+
+/**
+ * Created by flyboss on 2018/4/28.
+ */
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //设置响应内容类型
+        resp.setContentType("text/html");
+
+        //设置逻辑实现
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>" + "hello" + "</h1>");
+    }
+}
